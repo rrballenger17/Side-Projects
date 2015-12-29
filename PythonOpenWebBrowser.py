93831ff10123dd93fe9d649a344ac3c9efc9aae0
Ryan Ballenger <ballenger@g.harvard.edu>
2015-12-29 17:41:33 -0500

    edited open web browser

--- PythonOpenWebBrowser.py
+++ PythonOpenWebBrowser.py
@@ -1,6 +1,16 @@
 
+# prompts user to take a break by opening a web browser to play jazz music
+# The duration variable would be set to 2 * 60 * 60 to prompt every 2 hours
+
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("http://www.facebook.com")
+total_breaks = 3
+break_count = 0
+duration = 10 # 2 * 60 * 60
+
+print("This program started on " + time.ctime())
+while(break_count < total_breaks):
+	time.sleep(duration)
+	webbrowser.open("https://www.youtube.com/watch?v=0gcu3GI3nA4")
+	break_count = break_count + 1
